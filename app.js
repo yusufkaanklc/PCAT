ef98989c0ebf8cbbaca549de6da91365a82f8ef1
yusuf kağan <77469229+yusufkaanklc@users.noreply.github.com>
2024-01-16 18:57:38 +0300

    update

--- app.js
+++ app.js
@@ -1,19 +1,60 @@
 const express = require('express');
-
-const app = express();
+const readline = require('readline');
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).set({ 'Content-Type': 'text/html' });
-  const photo = {
-    id: 1,
-    name: 'photo name',
-    desc: 'photo description',
-  };
-  res.send(photo);
+const app = express();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda çalışıyor`);
-});
+const soru = (quest) => {
+  return new Promise((resolve, reject) => {
+    rl.question(quest, (answer) => {
+      if (!answer) {
+        console.log('Boş bırakılamaz. Tekrar giriniz:');
+        soru(quest).then(resolve);
+      } else {
+        resolve(answer);
+      }
+    });
+  });
+};
+
+const server = (data, endpoint) => {
+  return new Promise((resolve, reject) => {
+    if (endpoint) {
+      app.get(`/${endpoint}`, (req, res) => {
+        res.status(200).set({ 'Content-Type': 'text/plain' }).send(data);
+      });
+
+      app.get('/', (req, res) => {
+        res.redirect(`/${endpoint}`);
+      });
+
+      app.listen(port, () => {
+        console.log(`Server ${port} portunda çalışıyor`);
+      });
+      resolve();
+    } else {
+      reject('Endpoint yok');
+    }
+  });
+};
+
+const main = async () => {
+  try {
+    const data = await soru('İçerik giriniz:');
+    const endpoint = await soru('Endpoint giriniz:');
+
+    await server(data, endpoint);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    rl.close();
+  }
+};
+
+main();
