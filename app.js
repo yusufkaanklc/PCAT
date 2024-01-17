5a019059bfd5dd422ce08409692aea68bdfe4dc0
yusufkaanklc <yusufkaankilic.yk@gmail.com>
2024-01-17 17:52:56 +0300

    middleware

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+
+// middleware istek cevap döngüsünün arasında ek işlemler yapmak için kullanılır
+
+// const myLogger = (req, res, next) => {
+//   console.log('Middleware Log 1');
+//   next(); // next ile bir sonraki middleware çalışır
+// };
+
+// app.use(myLogger);
+
+app.use(express.static('public')); // burada middleware ile dosyalarımızı statik olarak yükledik
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).set({ 'Content-Type': 'text/html' });
-  const photo = {
-    id: 1,
-    name: 'photo name',
-    desc: 'photo description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
 app.listen(port, () => {
