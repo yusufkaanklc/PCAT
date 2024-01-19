2bf3433640fd8756ad622e897d9b8d263cc68589
yusuf kağan <77469229+yusufkaanklc@users.noreply.github.com>
2024-01-19 22:00:45 +0300

    database data creating

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
+const mongoose = require('mongoose');
 const app = express();
 
-let imageObject = {};
+mongoose
+  .connect('mongodb://127.0.0.1:27017/pcat')
+  .then(console.log('MongoDB connected'))
+  .catch((err) => console.log("MongoDB'e baglanırken hata oluştu: ", err));
 
 // Template Engine
 app.set('view engine', 'ejs');
@@ -15,25 +20,27 @@ app.use(express.json()); // burada istekteki json verilerini okumak için gerekl
 const port = 3000;
 
 // Routing
-app.get('/', (req, res) => {
-  res.render('index'); // burada index.ejs dosyasını render ediyoruz
+app.get('/', async (req, res) => {
+  try {
+    const photos = await Photo.find({});
+    res.render('index', { photos }); // burada index.ejs dosyasını render ediyoruz
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Sunucu hatası');
+  }
 });
 
 app.get('/about', (req, res) => {
-  res.render('about'); // burada index.ejs dosyasını render ediyoruz
+  res.render('about'); // burada about.ejs dosyasını render ediyoruz
 });
 
 app.get('/add', (req, res) => {
-  res.render('add');
+  res.render('add'); // burada add.ejs dosyasını render ediyoruz
 });
 
 app.post('/photos', async (req, res) => {
   try {
-    imageSave = () => {
-      imageObject = req.body;
-    };
-    await imageSave();
-    console.log(imageObject);
+    await Photo.create(req.body);
     res.redirect('/add');
   } catch (error) {
     console.log('Resim kaydetme hatası: ' + error);
